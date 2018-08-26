77652aed8d6292daadbbaf39a9bee46daa229820
Josh Binuya <joshtaborbinuya@gmail.com>
2018-08-26 11:08:07 -0600

    final

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Western",false);
+
+INSERT INTO burgers(burger_name,devoured)
+VALUES("Bacon Cheeseburger",false);
+
+INSERT INTO burgers(burger_name,devoured)
+VALUES("Basic Hamburger",false);
